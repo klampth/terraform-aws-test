00faf634abc07c50788f6f2a90456323f32eb919
Klampth Morpheus <klampth@gmail.com>
2023-01-28 20:03:46 +0000

    Import ec2 instance

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ provider "aws" {
 #export AWS_SECRET_KEY_ID=#Don't forget to export it
 #    access_key = "AKIAUONSTLJCLSKFFGKH"
 #    secret_key = "vliHDz2ooixHLpHf4kwUNkzPHDz4KhUpzdLHKQxD"
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  tags          = {
+    Name          = "terra_test_pmanchev-01"
+    Environment   = "Dev"
+    force_destroy = true
+  }
+}
 resource "aws_vpc" "b" {
 
     cidr_block = "10.11.0.0/16"
